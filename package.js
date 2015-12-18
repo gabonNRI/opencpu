dded331e01821e42916f781b4f0fc952044ba77d
SR Melody <srmelody@gmail.com>
2015-12-17 22:41:20 -0500

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'srmelody:opencpu',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Meteor support for opencpu to run R functions',
   // URL to the Git repository containing the source code for this package.
